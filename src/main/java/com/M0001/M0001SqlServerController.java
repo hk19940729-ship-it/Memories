9464c7d1426a9f0bb0135d9c6d2b8e8b122c7912
HIGHTDRIVE\hk199 <hk19940729@gmail.com>
2026-04-05 18:31:23 +0900

    バリデーション追加

--- src/main/java/com/M0001/M0001SqlServerController.java
+++ src/main/java/com/M0001/M0001SqlServerController.java
@@ -1,7 +1,11 @@
 package com.M0001;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,50 +14,40 @@ import com.Entities.HINEntity;
 import com.M0001.model.M0001;
 import com.M0001.services.M0001Service;
 
-
 @Controller
 public class M0001SqlServerController {
+
     @Autowired
     M0001Service M0001Service;
 
-    
     @GetMapping("/")
     public String Index(Model model) {
-        //model.addAttribute("title", "SQL Serverから取得したデータ");
-        // 品目データの取得
-    	model.addAttribute("M0001", new M0001()); // M0001はフォーム用のクラス index.htmlで参照するので生成が必要
-        //model.addAttribute("weatherDataList", weatherDataList);
+        model.addAttribute("M0001", new M0001());
         return "index";
     }
-    
+
     @PostMapping("/Search")
-    public String getHin(@ModelAttribute M0001 m001,Model model) {
-        //model.addAttribute("title", "SQL Serverから取得したデータ");
-        // 品目データの取得
-    	
-    		String hincode = m001.getHinCode();
-        HINEntity result = M0001Service.findbyId(hincode);
-        
-        if(result != null) {
-        	  m001.ApplyEdit(result,true);  	
+    public String getHin(@Valid @ModelAttribute("M0001") M0001 m001, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("M0001", m001);
+            return "index";
         }
-      
+
+        String hincode = m001.getHinCode();
+        HINEntity hinentity = M0001Service.findbyId(hincode);
+
+        if (hinentity != null) {
+            m001.ApplyEdit(hinentity, true);
+        }
+
         model.addAttribute("M0001", m001);
-         
-        //model.addAttribute("weatherDataList", weatherDataList);
         return "index";
     }
-    
 
     @PostMapping("/Save")
-    public String saveHin(@ModelAttribute M0001 m001,Model model) {
-        // 品目データの登録
-    		M0001Service.saveHin(m001);
-     	model.addAttribute("M0001", m001); 
-        //model.addAttribute("weatherDataList", weatherDataList);
+    public String saveHin(@ModelAttribute("M0001") M0001 m001, Model model) {
+        M0001Service.saveHin(m001);
+        model.addAttribute("M0001", m001);
         return "index";
     }
-    
-    
-    
 }
--- src/main/java/com/M0001/model/M0001.java
+++ src/main/java/com/M0001/model/M0001.java
@@ -2,138 +2,132 @@ package com.M0001.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 import com.Entities.HINEntity;
 
 @Entity
 public class M0001 {
-		@Id
-	    private String hinCode;
-	    private String hinName;
-	    public String hinName2;
-	    public String hinKbn;
-	    private String tani;
-	    private Double tanka;
-	    private Double readTime;
-	    private String colorCode;
-	    private Double syohikigenDays;
-	    private Boolean isNew = false;
-	    
+    @Id
+    @NotBlank(message = "品目コードは必須です")
+    private String hinCode;
 
-	    public String getHinCode() {
-	        return hinCode;
-	    }
-	    public void setHinCode(String hincode) {
-	        this.hinCode = hincode;
-	    }
+    private String hinName;
+    private String hinName2;
+    private String hinKbn;
+    private String tani;
+    private Double tanka;
+    private Double readTime;
+    private String colorCode;
+    private Double syohikigenDays;
+    private Boolean isNew = false;
 
-	    public String getHinName() {
-	        return hinName;
-	    }
-	    public void setHinName(String hinname) {
-	        this.hinName = hinname;
-	    }
-	    
-	    public String getHinName2() {
-	        return hinName2;
-	    }
-	    public void setHinNme2(String hinname2) {
-	        this.hinName2 = hinname2;
-	    }
-	    
-	    // Getter and Setter for hINbn
-	    public String getHINbn() {
-	        return hinKbn;
-	    }
+    public String getHinCode() {
+        return hinCode;
+    }
 
-	    public void setHINbn(String hINbn) {
-	        this.hinKbn = hINbn;
-	    }
+    public void setHinCode(String hincode) {
+        this.hinCode = hincode;
+    }
 
-	    // Getter and Setter for tani
-	    public String getTani() {
-	        return tani;
-	    }
+    public String getHinName() {
+        return hinName;
+    }
 
-	    public void setTani(String tani) {
-	        this.tani = tani;
-	    }
+    public void setHinName(String hinname) {
+        this.hinName = hinname;
+    }
 
-	    // Getter and Setter for tanka
-	    public Double getTanka() {
-	        return tanka;
-	    }
+    public String getHinName2() {
+        return hinName2;
+    }
 
-	    public void setTanka(Double tanka) {
-	        this.tanka = tanka;
-	    }
+    public void setHinName2(String hinname2) {
+        this.hinName2 = hinname2;
+    }
 
-	    // Getter and Setter for readTime
-	    public Double getReadTime() {
-	        return readTime;
-	    }
+    public String getHinKbn() {
+        return hinKbn;
+    }
 
-	    public void setReadTime(Double readTime) {
-	        this.readTime = readTime;
-	    }
+    public void setHinKbn(String hinKbn) {
+        this.hinKbn = hinKbn;
+    }
 
-	    // Getter and Setter for colorCode
-	    public String getColorCode() {
-	        return colorCode;
-	    }
+    public String getTani() {
+        return tani;
+    }
 
-	    public void setColorCode(String colorCode) {
-	        this.colorCode = colorCode;
-	    }
+    public void setTani(String tani) {
+        this.tani = tani;
+    }
 
-	    // Getter and Setter for syohikigenDays
-	    public Double getSyohikigenDays() {
-	        return syohikigenDays;
-	    }
+    public Double getTanka() {
+        return tanka;
+    }
 
-	    public void setSyohikigenDays(Double syohikigenDays) {
-	        this.syohikigenDays = syohikigenDays;
-	    }
-	    
-	
-		public Boolean getIsNew() {
-			return isNew;
-		}
-		public void setIsNew(Boolean isNew) {
-			this.isNew = isNew;
-		}
-	    
-		public void ApplyEdit(HINEntity hinenyity,Boolean isNew) {
-			if (isNew) {
-			    this.hinCode = hinenyity.getHinCode();
-			    this.isNew = true;
-			}
-			    this.hinName = hinenyity.getHinName();
-			    this.hinName2 = hinenyity.getHinName2();
-			    this.hinKbn = hinenyity.getHinKbn();
-			    this.tani = hinenyity.getTni();
-			    this.tanka = hinenyity.getTanka();
-			    this.readTime = hinenyity.getRt();
-			    this.colorCode = hinenyity.getColorCode();
-			    this.syohikigenDays = hinenyity.getSyohiKigenDays();
-			  
-		}
-		
-		public HINEntity toHINEntity() {
-		    HINEntity entity = new HINEntity();
-		    entity.setHinCode(this.hinCode);
-		    entity.setHinName(this.hinName);
-		    entity.setHinName2(this.hinName2);
-		    entity.setHinKbn(this.hinKbn);
-		    entity.setTni(this.tani);
-		    entity.setTanka(this.tanka);
-		    entity.setRt(this.readTime);
-		    entity.setColorCode(this.colorCode);
-		    entity.setSyohiKigenDays(this.syohikigenDays);
-		    return entity;
-		}
+    public void setTanka(Double tanka) {
+        this.tanka = tanka;
+    }
 
-	
+    public Double getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(Double readTime) {
+        this.readTime = readTime;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public Double getSyohikigenDays() {
+        return syohikigenDays;
+    }
+
+    public void setSyohikigenDays(Double syohikigenDays) {
+        this.syohikigenDays = syohikigenDays;
+    }
+
+    public Boolean getIsNew() {
+        return isNew;
+    }
+
+    public void setIsNew(Boolean isNew) {
+        this.isNew = isNew;
+    }
+
+    public void ApplyEdit(HINEntity hinenyity, Boolean isNew) {
+        if (isNew) {
+            this.hinCode = hinenyity.getHinCode();
+            this.isNew = true;
+        }
+        this.hinName = hinenyity.getHinName();
+        this.hinName2 = hinenyity.getHinName2();
+        this.hinKbn = hinenyity.getHinKbn();
+        this.tani = hinenyity.getTni();
+        this.tanka = hinenyity.getTanka();
+        this.readTime = hinenyity.getRt();
+        this.colorCode = hinenyity.getColorCode();
+        this.syohikigenDays = hinenyity.getSyohiKigenDays();
+    }
+
+    public HINEntity toHINEntity() {
+        HINEntity entity = new HINEntity();
+        entity.setHinCode(this.hinCode);
+        entity.setHinName(this.hinName);
+        entity.setHinName2(this.hinName2);
+        entity.setHinKbn(this.hinKbn);
+        entity.setTni(this.tani);
+        entity.setTanka(this.tanka);
+        entity.setRt(this.readTime);
+        entity.setColorCode(this.colorCode);
+        entity.setSyohiKigenDays(this.syohikigenDays);
+        return entity;
+    }
 }
-
-
